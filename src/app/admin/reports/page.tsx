2c4bc8cc4fac94e2bff14cd080d93faac79d0b68
Angelica Eva <evaangelica456@gmail.com>
2025-10-27 10:47:47 +0800

    new changes

--- src/app/admin/reports/page.tsx
+++ src/app/admin/reports/page.tsx
@@ -738,10 +738,10 @@ const ReportsPage = () => {
                 <SelectContent className="rounded-xl">
                   <SelectItem value="6">All ratings</SelectItem>
                   <SelectItem value="5">5★</SelectItem>
-                  <SelectItem value="4">4★+</SelectItem>
-                  <SelectItem value="3">3★+</SelectItem>
-                  <SelectItem value="2">2★+</SelectItem>
-                  <SelectItem value="1">1★+</SelectItem>
+                  <SelectItem value="4">4★</SelectItem>
+                  <SelectItem value="3">3★</SelectItem>
+                  <SelectItem value="2">2★</SelectItem>
+                  <SelectItem value="1">1★</SelectItem>
                 </SelectContent>
               </Select>
             </div>
